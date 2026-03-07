cfa4c9cd5b6cd7de168527e88881a510cef8f89c
Ivan Semkin <ivan@semkin.ru>
2026-03-07 17:42:03 +0300

    Add missing `built_collection` dependency to `serializers.dart`

--- lib/serializers.dart
+++ lib/serializers.dart
@@ -1,3 +1,4 @@
+import 'package:built_collection/built_collection.dart';
 import 'package:built_value/serializer.dart';
 import 'package:built_value/standard_json_plugin.dart';
 import 'package:national_identic/entity/country.dart';
